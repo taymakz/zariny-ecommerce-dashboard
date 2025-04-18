691e26fc54d8a8f6bf69a4c6462976042f3071f1
Taymakz <taymazak1382@gmail.com>
2025-04-18 18:02:05 +0330

    feat: improve badge variants

--- app/components/ui/badge/index.ts
+++ app/components/ui/badge/index.ts
@@ -4,26 +4,23 @@ import { cva } from 'class-variance-authority'
 export { default as Badge } from './Badge.vue'
 
 export const badgeVariants = cva(
-  'inline-flex items-center rounded-full border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 select-none',
+  'inline-flex items-center rounded-full border px-2.5 py-0.5 text-xs transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 select-none',
   {
     variants: {
       variant: {
-        'default':
-          'border-transparent bg-primary text-primary-foreground hover:bg-primary/80',
-        'secondary':
+        default:
+          'bg-brand hover:bg-brand/90 text-white dark:bg-brand/15 dark:text-brand-lighter border-transparent dark:hover:bg-brand/20',
+        secondary:
           'border-transparent bg-secondary text-secondary-foreground hover:bg-secondary/80',
-        'destructive':
-          'border-transparent bg-destructive text-destructive-foreground hover:bg-destructive/80',
-        'outline': 'text-foreground',
+        outline: 'text-foreground',
 
-        'glass-primary':
-          'border-transparent bg-primary/15 text-brand-lighter hover:bg-primary/20',
-        'glass-success':
-          'border-transparent bg-success/15 text-success-lighter hover:bg-success/20',
-        'glass-alert':
-          'border-transparent bg-alert/15 text-alert-lighter hover:bg-alert/20',
-        'glass-warning':
-          'border-transparent bg-warning/15 text-warning-lighter hover:bg-warning/20',
+        info: 'bg-info hover:bg-info/90 text-white dark:bg-info/15 dark:text-info-lighter border-transparent dark:hover:bg-info/20',
+
+        success: 'bg-success hover:bg-success/90 text-white dark:bg-success/15 dark:text-success-lighter border-transparent dark:hover:bg-success/20',
+
+        alert: 'bg-alert hover:bg-alert/90 text-white dark:bg-alert/15 dark:text-alert-lighter border-transparent dark:hover:bg-alert/20',
+
+        warning: 'bg-warning hover:bg-warning/90 text-white dark:bg-warning/10 dark:text-warning-lighter border-transparent dark:hover:bg-warning/15',
       },
     },
     defaultVariants: {
